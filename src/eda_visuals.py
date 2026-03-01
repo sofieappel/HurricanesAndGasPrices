e7ea9dfa7b5981500752d469d778ae57f033a791
eenoonan <eenoonan00@gmail.com>
2026-03-01 08:09:59 -0700

    Refactor gas_visuals and add overview function

--- src/eda_visuals.py
+++ src/eda_visuals.py
@@ -1,6 +1,8 @@
 # eda_visuals.py
 
 def hurr_visuals(hurr_sum_list):
+    print(f'Starting EDA for {hurr_sum_list.name}')
+    
     import seaborn as sns
     import matplotlib.pyplot as plt
     import pandas as pd
@@ -46,23 +48,75 @@ def hurr_visuals(hurr_sum_list):
     plt.ylabel('Count of Storms')
     plt.show()
 
-def gas_visuals(gas_data_redux):
+def gas_visuals_ov(gas_data, metro_grp):
+    print(f'Starting overview for {gas_data.name} and {metro_grp}')
     import seaborn as sns
     import matplotlib.pyplot as plt
     import pandas as pd
 
+    gas_data_redux = gas_data[(gas_data['metro'].isin(metro_grp))]
+
+    min_year = min(gas_data_redux['year'])
+    max_year = max(gas_data_redux['year'])
+
+    # Time Hist of gas prices by locality
+    plt.figure(figsize=(20, 10))
+    sns.lineplot(data=gas_data_redux, x='date', y='regular', hue='metro')
+    # plt.axvspan(datetime.date(2025, 6, 1), datetime.date(2025, 11, 30), alpha = 0.3)
+    plt.title(f'Regular Gas Price ({min_year}-{max_year})')
+    plt.xlabel('Date')
+    plt.ylabel('Gas Price (Regular)')
+    plt.show()
+
+    # Distribution of gas prices in reporting period
+    #plt.figure(figsize=(20, 10))
+    sns.histplot(data=gas_data_redux, x='regular')
+    plt.title(f'Regular Gas Price Distribution ({min_year}-{max_year})')
+    plt.xlabel('Date')
+    plt.ylabel('Gas Price (Regular)')
+    plt.show()
+
+    # Distribution of gas prices by locality
+    #plt.figure(figsize=(20, 10))
+    sns.displot(data=gas_data_redux, x='regular', hue='metro')
+    plt.suptitle(f'Regular Gas Price Distribution by Metro Area ({min_year}-{max_year})')
+    plt.xlabel('Date')
+    plt.ylabel('Gas Price (Regular)')
+    plt.show()
+
+def gas_visuals(gas_data, metro_grp):
+    print(f'Starting EDA for {gas_data.name} and {metro_grp}')
+    import seaborn as sns
+    import matplotlib.pyplot as plt
+    import pandas as pd
+
+    gas_data_redux = gas_data[(gas_data['metro'].isin(metro_grp))]
+
+    min_year = min(gas_data_redux['year'])
+    max_year = max(gas_data_redux['year'])
+
     # Histograms of gas prices by locality
-    plt.figure(figsize=(20, 5))
-    sns.displot(data=gas_data_redux, x='regular', col='metro', col_wrap=4)
-    plt.title(f'Regular Gas Price')
+    #plt.figure(figsize=(20, 10))
+    sns.displot(data=gas_data_redux, x='regular', col='metro', col_wrap=3)
+    plt.suptitle(f'Regular Gas Price Distribution ({min_year}-{max_year})', y = 0.99)
+    plt.subplots_adjust(top=0.9)
+    plt.xlabel('Date')
+    plt.ylabel('Gas Price (Regular)')
+    plt.show()
+
+    # Histograms of gas prices by locality and year
+    #plt.figure(figsize=(20, 10))
+    sns.displot(data=gas_data_redux, x='regular', col='metro', hue='year', col_wrap=3)
+    plt.suptitle(f'Regular Gas Price Distribution by Year ({min_year}-{max_year})', y=0.99)
+    plt.subplots_adjust(top=0.9)
     plt.xlabel('Date')
     plt.ylabel('Gas Price (Regular)')
     plt.show()
 
     # Time Hist of gas prices by locality
-    plt.figure(figsize=(20, 5))
-    sns.lineplot(data=gas_data_redux, x='date', y='regular', hue='metro')
-    plt.title(f'Regular Gas Price')
+    #plt.figure(figsize=(20, 5))
+    sns.displot(data=gas_data_redux, x='regular', hue='metro')
+    plt.suptitle(f'Regular Gas Price ({min_year}-{max_year})')
     plt.xlabel('Date')
     plt.ylabel('Gas Price (Regular)')
     plt.show()
